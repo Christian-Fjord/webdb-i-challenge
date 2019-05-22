e38627a30fdfaca7332043b6feeccc0b5bb6f16b
Christian-Fjord <fjord.christian@gmail.com>
2019-05-22 15:15:21 -0500

    Added Express || Added Get endpoint

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 const express = require('express');
-
 const server = express();
 
-// your code here
+const db = require('./data/accounts-model');
+
+server.use(express.json());
+
+
+// Get
+
+server.get('/api/accounts', (req, res) => {
+  db.get()
+    .then( get => {
+      res.status(200).json(get)
+    })
+    .catch( error => {
+      res.status(500).json({error:{message: "The item(s) you tried to get could not be gotten."}})
+    })
+} )
+
+
+
 
 module.exports = server;
