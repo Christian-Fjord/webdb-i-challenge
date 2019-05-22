d26c7cbb74243e9bf1a4bccdf70f3dcb03c1e4f0
Christian-Fjord <fjord.christian@gmail.com>
2019-05-22 15:15:50 -0500

    Added GetByID endpoint

--- server.js
+++ server.js
@@ -18,7 +18,19 @@ server.get('/api/accounts', (req, res) => {
     })
 } )
 
+// Get By ID
 
+server.get('/api/accounts/:id', (req, res) => {
+  const id = req.params.id
+
+  db.getById(id)
+    .then( getId => {
+      res.status(200).json(getId)
+    })
+    .catch( error => {
+      res.status(500).json({error:{message: ' The ID could not be found.'}})
+    })
+})
 
 
 module.exports = server;
